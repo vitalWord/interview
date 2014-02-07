f0eb16fc16dbe62729da44d164f5e435577c9c8c
Sanjo <Deundiak@gmail.com>
2014-02-07 22:15:49 +0200

    Redone first task

--- 1.cpp
+++ 1.cpp
@@ -1,9 +1,17 @@
 // –азверните строку. ”казатель reverse_string должен указывать на развернутую строку.
 
-char* string = УThe string!Ф;
+#include <cstring>
+
+char* string = "The string!";
 int main()
 {
-	char* reverse_string = "!gnirts ehT";
+
+    size_t length = strlen(string);
+	char* reverse_string = new char[length + 1];
+	reverse_string[length] = '\0';
+
+	for (size_t i=0; i < length; i++)
+        reverse_string[i] = string[length-i-1];
 
 	return 0;
 }
