d9f484ace45a406281f2759726f0a9f933e75361
ameks94 <ameks94@gmail.com>
2014-02-07 23:04:27 +0200

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,11 +1,16 @@
-﻿// Разверните строку. Указатель reverse_string должен 
-// указывать на развернутую строку.
+#include <iostream>
+#include <string>
 
 char* string = "The string!";
-
 int main()
 {
-	char* reverse_string;
-
+	char* reverse_string = new char[strlen(string)];
+	for(int i = 0 ; i < strlen(string); i++)
+	{
+		reverse_string[i] = string[strlen(string)-1 - i];
+	}
+	reverse_string[strlen(string)] = '\0';
+	std::cout << reverse_string;
+	system("pause");
 	return 0;
 }
