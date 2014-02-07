750e4e12b3e9c3475dc569b4b2028d2ec4692b40
Denis Polyakov <den1s.polyakov1@gmail.com>
2014-02-07 23:27:08 +0200

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -1,23 +1,50 @@
-﻿// Напишите функцию Add(), которая создает новый 
-// объект List, инициализирует его входным значением 
-// value и добавляет его в конец списка l, полученного 
-// на вход. В функции main() создайте проинициализированный 
-// список, со значениями value равными: 1, 2, 3, 4 и 5.
+#include <iostream>
 
 struct List
 {
-      int value;
-      List* next;
+	int value;
+	List* next;
 };
 
 // Add should create new List object, initialize it by value and add it to the end of the list.
 // It should return pointer to the added List object.
 List* Add(List* l, int value)
 {
+	List * newListObject = new List;
+	newListObject->next = NULL;
+	newListObject->value = value;
 
+	List * temp = l;	
+	while (temp != NULL)
+	{
+		if (temp->next == NULL)
+		{
+			temp->next = newListObject;
+			return newListObject;
+		}
+		temp = temp->next;
+	}
+}
+
+void display(List * head) {
+	List * list = head;
+	while (list != NULL) {
+		std::cout << list->value << " ";
+		list = list->next;
+	}
 }
 
 int main(int argc, char* argv[])
 {
-return 0;
+	List * list = new List;
+	list->value = 1;
+	list->next = NULL;
+
+	for (int i = 2; i <= 5; i++)
+	{
+		Add(list, i);
+	}
+
+	display(list);
+	return 0;
 }
