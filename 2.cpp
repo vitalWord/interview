f2d08566f73b5233b355554a321b7cbcba34ac8b
Denis Polyakov <den1s.polyakov1@gmail.com>
2014-02-07 23:26:53 +0200

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,19 +1,22 @@
-﻿// Квадратная матрица разделена диагоналями на четыре сектора. 
-// Напишите функцию min_from_top_sector(), которая будет 
-// находить значение ячейки, минимальное для всех ячеек верхнего 
-// сектора, включая отрезки диагоналей, составляющие этот сектор.
-
 #include <iostream>
 
 const int c_kM = 5;
 typedef int Matrix[c_kM][c_kM];
 
-int min_from_top_sector(Matrix& m)
+int min_from_top_sector(Matrix & m)
 {
-
+	int min = m[0][0];
+	for (int i = 0; i < c_kM / 2; i++)
+	{
+		for (int j = i; j < c_kM - i; j++)
+		{
+			if (m[i][j] < min) min = m[i][j];
+		}
+	}
+	return min;
 }
 
-int _tmain(int argc, _TCHAR* argv[])
+int main()
 {
 	Matrix matrix;
 
