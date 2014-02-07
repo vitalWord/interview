b2b6b96c171b1d41b76d622fbde24088e5ba34d6
Youw <ihor.youw@gmail.com>
2014-02-07 22:28:08 +0200

    first done

--- 1.cpp
+++ 1.cpp
@@ -7,5 +7,14 @@ int main()
 {
 	char* reverse_string;
 
+	//для завдання нема потреба оформляти у вигляді функції
+	int len = strlen(string);
+	reverse_string = new char[len + 1];
+	for (int i = 0; i < len; i++) {
+		reverse_string[i] = string[len - i - 1];
+	}
+	reverse_string[len] = 0;
+	//
+	
 	return 0;
 }
