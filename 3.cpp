bc3de963a82f23bf91e5f29eaf309455195d99e0
ameks94 <ameks94@gmail.com>
2014-02-07 23:05:23 +0200

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -1,9 +1,8 @@
-﻿// Напишите функцию Add(), которая создает новый 
+// Напишите функцию Add(), которая создает новый 
 // объект List, инициализирует его входным значением 
 // value и добавляет его в конец списка l, полученного 
 // на вход. В функции main() создайте проинициализированный 
 // список, со значениями value равными: 1, 2, 3, 4 и 5.
-
 struct List
 {
       int value;
@@ -14,10 +13,17 @@ struct List
 // It should return pointer to the added List object.
 List* Add(List* l, int value)
 {
-
+	List *list = new List;
+	list->value = value;
+	l->next = list;
+	return list;
 }
 
 int main(int argc, char* argv[])
 {
-return 0;
+	List * list = new List;
+	list->value = 1;
+	for (int i = 1; i < 5; i++)
+		Add(list,i+1);
+	return 0;
 }
