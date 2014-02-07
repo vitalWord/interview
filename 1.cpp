7813b2b19bb767df547f6b2eebe6e591baa23844
Vadym Drozd <Vadym@Vadym-NetPC.(none)>
2014-02-07 21:39:16 +0200

    First

--- 1.cpp
+++ 1.cpp
@@ -1,9 +1,30 @@
-// –азверните строку. ”казатель reverse_string должен указывать на развернутую строку.
+#include <stdio.h>
+#include <string.h>
 
-char* string = УThe string!Ф;
+static const char* str = "The string!";
+
+void strreverse(char * aStr);
+
+#define swap_char(left, right) \
+	do {                       \
+		left = left ^ right;   \
+		right = left ^ right;  \
+		left = left ^ right;   \
+	} while (0)
+
 int main()
 {
-	char* reverse_string;
-
+	char* reverse_string = strdup(str);
+	printf("%s\n", str);
+	strreverse(reverse_string);
+	printf("%s\n", reverse_string);
 	return 0;
+}
+
+void strreverse(char * p)
+{
+  char *q = p;
+  q += strlen(p);
+  for(--q; p < q; ++p, --q)
+  	swap_char(*p, *q);
 }
