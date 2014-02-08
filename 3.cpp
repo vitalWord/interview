725eb1fa147be27f511754a1006ce3e13eacef69
zagrichanskiy <zagrichanskiy@gmail.com>
2014-02-08 11:24:57 +0200

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -4,6 +4,8 @@
 // на вход. В функции main() создайте проинициализированный 
 // список, со значениями value равными: 1, 2, 3, 4 и 5.
 
+#define     NULL  0
+
 struct List
 {
       int value;
@@ -12,12 +14,30 @@ struct List
 
 // Add should create new List object, initialize it by value and add it to the end of the list.
 // It should return pointer to the added List object.
-List* Add(List* l, int value)
+List* Add(List* beg, int value)
 {
-
+      List* new_item = new List;
+      new_item->value = value;
+      new_item->next = NULL;
+      
+      if (beg) {
+            List* temp = beg;
+            while (temp->next)
+                  temp = temp->next;
+            temp->next = new_item;
+      }
+      
+      return new_item;
 }
 
 int main(int argc, char* argv[])
 {
-return 0;
+      List* begin = NULL;
+      begin = Add(begin, 1);
+      Add(begin, 2);
+      Add(begin, 3);
+      Add(begin, 4);
+      Add(begin, 5);
+      
+      return 0;
 }
