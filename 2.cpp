b5cb0d959490a8f12e05a2be235b39664cbfa33d
Denis Polyakov <den1s.polyakov1@gmail.com>
2014-02-08 14:34:42 +0200

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -6,7 +6,7 @@ typedef int Matrix[c_kM][c_kM];
 int min_from_top_sector(Matrix & m)
 {
 	int min = m[0][0];
-	for (int i = 0; i < с_kM % 2 + c_kM / 2; i++)
+	for (int i = 0; i < c_kM % 2 + c_kM / 2; i++)
 	{
 		for (int j = i; j < c_kM - i; j++)
 		{
