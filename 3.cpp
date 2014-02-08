31ab1a3df36d1f3cfb1499d89038f880651571f0
SanjoDeundiak <Deundiak@gmail.com>
2014-02-08 15:34:28 +0200

    Added destructor

--- 3.cpp
+++ 3.cpp
@@ -8,7 +8,13 @@ struct List
 {
       int value;
       List* next;
+      ~List();
 };
+
++List::~List() {
+ +    if (this->next)
+ +        delete this->next;
+  }
 
 // Add should create new List object, initialize it by value and add it to the end of the list.
 // It should return pointer to the added List object.
@@ -28,5 +34,7 @@ int main(int argc, char* argv[])
 {
     List *begin = Add(NULL, 1);
     Add(Add(Add(Add(begin, 2), 3), 4), 5);
+    
+    delete begin;
     return 0;
 }
