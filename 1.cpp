8fe41ac99c3259c5d33d51b955565f5baaf174a9
Vadym Drozd <Vadym@Vadym-NetPC.(none)>
2014-02-09 02:41:22 +0200

    Added second implementation of first task

--- 1.cpp
+++ 1.cpp
@@ -1,29 +1,30 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#define USE_VERSION 2
 
 static const char* str = "The string!";
 
+#if (USE_VERSION == 1)
+
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+
 void strreverse(char * aStr);
 
-#define swap_char(left, right) \
-	do {                       \
-		left = left ^ right;   \
-		right = left ^ right;  \
-		left = left ^ right;   \
-	} while (0)
+# define swap_char(left, right) { char _t = left; left = right; right = _t; }
 	
-#ifdef _MSC_VER
-# define strdup _strdup
-#endif
+# ifdef _MSC_VER
+#  define strdup _strdup
+# endif
 
 int main()
 {
 	char* reverse_string = strdup(str); //strlen + alloc + memcpy
-	printf("%s\n", str);
-	strreverse(reverse_string);
-	printf("%s\n", reverse_string);
 
+	strreverse(reverse_string);
+
+	printf("%s\n", str);
+	printf("%s\n", reverse_string);
+	
 	free(reverse_string);
 	
 	return 0;
@@ -35,3 +36,27 @@ void strreverse(char * p)
   for(--q; p < q; ++p, --q)
   	swap_char(*p, *q);
 }
+
+#endif
+
+#if (USE_VERSION == 2)
+
+# include <iostream>
+# include <algorithm>
+	// std::reverse
+# include <string>
+
+int main () {
+  std::string result(str);
+
+  std::reverse(result.begin(), result.end());
+
+  char* reverse_string = &result[0];
+
+  std::cout << "Original: " << str << std::endl;
+  std::cout << "Reversed: " << reverse_string << std::endl;
+
+  return 0;
+}
+
+#endif
